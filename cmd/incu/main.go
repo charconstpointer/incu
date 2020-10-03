ae52cfc536460ab005b895302d78ec808ce963e3
charconstpointer <attwistingnether@gmail.com>
2020-10-03 23:07:46 +0200

    .

--- cmd/incu/main.go
+++ cmd/incu/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"log"
 	"os"
 	"time"
 )
@@ -22,16 +21,11 @@ type Current struct {
 }
 
 func main() {
-	// b, err := ioutil.ReadFile("./dalet.xml")
-	// if err != nil {
-	// 	log.Fatalln(err.Error())
-	// }
 	f, _ := os.Open("./dalet.xml")
 	d := xml.NewDecoder(f)
 	var bm BroadcastMonitor
 	d.Decode(&bm)
-	log.Println(bm.Current)
-	// 2020-10-02T14:07:18.944
+
 	t, err := time.Parse("2006-01-02T15:04:05", bm.Current.StartTime)
 
 	if err != nil {
